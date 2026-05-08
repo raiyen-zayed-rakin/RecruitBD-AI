eb854281856edc382b2464c67e0503f479c772c0
Md. Iftakhar Awal Chowdhury <iftakhar.awal@gmail.com>
2026-05-08 15:05:40 +0600

    feat(backend): add env `PORT` support

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,10 @@
+import os
 import uvicorn
 from dotenv import load_dotenv
 
 load_dotenv()
 
+PORT = os.getenv("PORT", 8000)
+
 if __name__ == "__main__":
-    uvicorn.run("api.app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("api.app:app", host="0.0.0.0", port=int(PORT), reload=True)
